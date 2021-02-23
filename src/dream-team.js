a0a7b8de85c73a4234d794c15d22c694abb44600
Alexey Fomenko <mrstatic23@yandex.ru>
2021-02-23 20:15:35 +0300

    feat: implement dream_team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let secretTeamName = [];
+  if (Array.isArray(members)) {
+    members.forEach(element => {
+      if (typeof element === 'string') {
+        const name = element.trim();
+        secretTeamName.push(name[0].toUpperCase());
+      }
+    });
+    return secretTeamName.sort().join('');
+  }
+
+  return false;
 };
