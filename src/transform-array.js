6eb993a88a7b424bc179511f57075f0e29290e61
Alexey Fomenko <mrstatic23@yandex.ru>
2021-02-28 12:16:02 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+
+  if (!Array.isArray(array)) {
+    throw new Error("Initial data is not array");
+}
+
+const tArray = [];
+const command = [ "--discard-next", "--discard-prev", "--double-next", "--double-prev" ]
+
+for (let i = 0; i < array.length; i++) {
+
+switch (command.find(e => e === array[i])) {
+  case "--discard-next":
+    if (i !== array.length) {
+      tArray.push(undefined);
+      i++;
+    }
+    break;
+  case "--discard-prev":
+    if (i !== 0) {
+      tArray.pop();
+    }
+    break;
+  case "--double-next":
+    if (i !== array.length) {
+      tArray.push(array[i + 1]);
+    }
+    break;
+  case "--double-prev":
+    if (i !== 0) {
+      tArray.push(tArray[tArray.length - 1]);
+    }
+    break;
+  default:
+    tArray.push(array[i])
+    continue;
+}
+}
+
+return tArray.filter(e => e !== undefined);
 };
