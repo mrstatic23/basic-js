2ec20bbb7a2c10ef20f18af0f5f5b99ec15d5f3c
Alexey Fomenko <mrstatic23@yandex.ru>
2021-03-06 12:29:14 +0300

    fix: add lost funtion declaration

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,13 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = calculateHanoi(disksNumber, turnsSpeed) {
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   const result = {
     turns: 0,
     seconds: 0,
   }
 
-  result.turns = Math.pow(2, disksNumber) - 1;
+  result.turns = Math.pow(2, disksNumber);
   result.seconds = Math.floor(result.turns / (turnsSpeed / 3600))  
 
   return result;
-};
+}
